9eb33035917c97541c8098aed62ecaeca3eaf5e8
Mason McCoy <mmasonmccoy@gmail.com>
2020-12-02 18:34:02 -0600

    Added python script to generate html data table.

--- WebVisualizations/weatherData.py
+++ WebVisualizations/weatherData.py
@@ -2,4 +2,9 @@ import pandas
 
 data = pandas.read_csv(".\Resources\cities.csv")
 weatherDF = pandas.DataFrame(data)
-pandas.DataFrame.to_html(weatherDF)
+htmlDF = pandas.DataFrame.to_html(weatherDF)
+print(htmlDF)
+
+table = open("dataTable.html", "w")
+table.write(htmlDF)
+table.close()
